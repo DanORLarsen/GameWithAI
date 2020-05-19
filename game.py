4366ac4102f0a7612c35ccd7159a9b9316a97f73
danorlarsen <johndoe@example.com>
2020-05-19 17:12:01 +0200

    Base game created

---
+++ game.py
@@ -0,0 +1,212 @@
+import pygame as pygame
+from threading import Thread
+##Inspiried by https://nerdparadise.com/programming/pygame
+width = 1200
+height = 800
+size = [width, height]
+
+
+pygame.init()
+screen = pygame.display.set_mode((size))
+done = False
+is_blue = True
+playerx = 30
+playery = 30
+enemyx = 400
+enemyy = 500
+goal = 1000
+myfont = pygame.font.SysFont("monospace", 16)
+myWinFont = pygame.font.SysFont("monospace", 45)
+WHITE = (255,255,255)
+clock = pygame.time.Clock()
+deaths = 0
+win = False
+speed = 3
+enemyMvoingUp = True
+
+
+
+#Metgods
+def die():
+    print("DEAD - Respawning")
+
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+                
+        pressed = pygame.key.get_pressed()
+        if (not win): 
+            if pressed[pygame.K_UP]: 
+                playery -= speed
+            if pressed[pygame.K_DOWN]: 
+                playery += speed
+
+            if pressed[pygame.K_LEFT]: 
+                playerx -= speed
+
+            if pressed[pygame.K_RIGHT]: 
+                playerx += speed
+        
+        
+       
+        screen.fill((220, 220, 220))
+        color = (0, 128, 255)
+        
+        #Goal
+        goalWall = pygame.draw.rect(screen, (0,255,0), pygame.Rect(goal + 50, 0, 500, height))
+        pygame.draw.line(screen, (0,255,0), (goal+100,0), (goal+100,height), 200)
+        
+
+        #Obstacles
+        #Boarder walls
+        wall1 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 0, width, 10))
+        wall2 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, height-10, width, 10))
+        wall3 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 0, 10, height))
+        wall4 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(width-10, 0, 10, height))
+
+        #Before middle
+        wall5 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 120, 200, 10))
+        wall6 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(200, 120, 10, 100))
+        wall7 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(100, 300, 275, 10))
+        wall8 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 380, 275, 10))
+        wall9 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 600, 370, 10))
+
+        #Middle pillars
+        wall10 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(370, 0, 10, 520))
+        wall11 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(370, 600, 10, 200))
+        wall12 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(470, 250, 10, 600))
+        wall13 = pygame.draw.rect(screen, (0,0,0), pygame.Rect(470, 0, 10, 175))
+
+        #Deaths
+        deathtext = myfont.render("Deaths {0}".format(deaths), 1, (0,0,0))
+        screen.blit(deathtext, (12, 10))
+
+        if (win):
+            winText = myWinFont.render("YOU WON", 1, (0,0,0))
+            screen.blit(winText, (500, 350))
+
+       #Player
+        player = pygame.draw.rect(screen, color, pygame.Rect(playerx, playery, 40, 40))
+        #Collide wall logic
+        if (player.colliderect(wall1)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall2)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall3)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall4)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall5)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall6)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall7)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall8)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall9)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall10)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall11)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall12)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(wall13)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        if (player.colliderect(goalWall)):
+            win = True
+
+        #Enemies
+        enemy = pygame.draw.rect(screen, (255,0,0), pygame.Rect(enemyx, enemyy, 50, 50))
+            #Collide with this enemy
+        if (player.colliderect(enemy)):
+            die()
+            deaths += 1
+            playerx = 30
+            playery = 30
+            enemyx = 400
+            enemyy = 500
+        
+    #ENEMY MOVEMENT
+        if(enemyMvoingUp):
+            enemyy -= 3
+        if(enemyy <= 20):
+            enemyMvoingUp = False
+        if(not enemyMvoingUp):
+            enemyy += 3
+        if(enemyy >= 720):
+            enemyMvoingUp = True
+
+
+        pygame.display.flip()
+        clock.tick(60)
+        pygame.display.flip() 
